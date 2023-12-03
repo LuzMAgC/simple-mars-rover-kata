df91517128b0c77b9b9fafad45bcd04d1cb047da
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 19:52:07 +0100

    refactor: remove magic consts

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,8 +1,11 @@
 class MarsRover:
-    direction = ["N", "E", "S", "W"]
+    directions = ["N", "E", "S", "W"]
+    grid_height = 10
+    move_forward_command = "M"
+    turn_right_command = "R"
 
     def execute(self, command: str) -> str:
-        y_axis = command.count("M") % 10
-        direction = self.direction[command.count("R") % len(self.direction)]
+        y_axis = command.count(self.move_forward_command) % self.grid_height
+        direction = self.directions[command.count(self.turn_right_command) % len(self.directions)]
 
         return "0:" + str(y_axis) + ":" + direction
