94f760202160b251a5982f76fc3673433ac7a75c
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 18:38:51 +0100

    refactor: execute return implementation

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,9 +1,3 @@
 class MarsRover:
     def execute(self, command: str) -> str:
-        if command == "M":
-            return "0:1:N"
-        if command == "MM":
-            return "0:2:N"
-        if command == "MMM":
-            return "0:3:N"
-        return "0:0:N"
+        return "0:"+str(len(command))+":N"
