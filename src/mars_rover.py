239656d5a3382279ebd5a18a1f03ef443371f231
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 19:16:13 +0100

    refactor: simplify rotate right with a list

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,9 +1,8 @@
 class MarsRover:
+    direction = ["N", "E", "S", "W"]
+
     def execute(self, command: str) -> str:
-        if command == "R":
-            return "0:0:E"
-        if command == "RR":
-            return "0:0:S"
-        if command == "RRR":
-            return "0:0:W"
+        if "R" in command:
+            return "0:0:" + self.direction[len(command)]
+
         return "0:"+str(len(command) % 10)+":N"
