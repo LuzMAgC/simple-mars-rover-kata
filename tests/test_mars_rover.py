758f2bd6a6c137406abb8df5dabbbc4596f68d00
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 18:58:05 +0100

    refactor: parametrize first wrap around test

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -18,10 +18,8 @@ class TestMarsRover:
     def test_command_forward_return(self, mars_rover, command, expected):
         assert mars_rover.execute(command) == expected
 
-    def test_command_forward_to_end_of_grid_should_wrap_around(self, mars_rover):
-        assert mars_rover.execute("MMMMMMMMMM") == "0:0:N"
-
     @pytest.mark.parametrize("command, expected", [
+        ("MMMMMMMMMM", "0:0:N"),
         ("MMMMMMMMMMM", "0:1:N"),
         ("MMMMMMMMMMMM", "0:2:N")
     ])
