fbc3d397c14e09c38a7bffdaf1993dd1a9ea4f7d
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 18:25:08 +0100

    refactor: mars rover fixture on tests

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -1,39 +1,20 @@
+import pytest
 from src.mars_rover import MarsRover
 
 
 class TestMarsRover:
-    def test_landing_position_returns_0_0_N(self):
-        # Given
-        mars_rover = MarsRover()
+    @pytest.fixture
+    def mars_rover(self):
+        return MarsRover()
 
-        # When
-
-        # Then
+    def test_landing_position_returns_0_0_N(self, mars_rover):
         assert mars_rover.execute("") == "0:0:N"
 
-    def test_command_forward_returns_0_1_N(self):
-        # Given
-        mars_rover = MarsRover()
-
-        # When
-
-        # Then
+    def test_command_forward_returns_0_1_N(self, mars_rover):
         assert mars_rover.execute("M") == "0:1:N"
 
-    def test_command_forward_twice_returns_0_2_N(self):
-        # Given
-        mars_rover = MarsRover()
-
-        # When
-
-        # Then
+    def test_command_forward_twice_returns_0_2_N(self, mars_rover):
         assert mars_rover.execute("MM") == "0:2:N"
 
-    def test_command_forward_three_times_returns_0_3_N(self):
-        # Given
-        mars_rover = MarsRover()
-
-        # When
-
-        # Then
+    def test_command_forward_three_times_returns_0_3_N(self, mars_rover):
         assert mars_rover.execute("MMM") == "0:3:N"
