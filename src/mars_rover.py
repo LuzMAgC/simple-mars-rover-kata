8df19421314ba0e78d45f062826472bcaee5d913
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 19:01:53 +0100

    test_rotate_right_returns_0_0_E

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -1,3 +1,5 @@
 class MarsRover:
     def execute(self, command: str) -> str:
+        if command == "R":
+            return "0:0:E"
         return "0:"+str(len(command) % 10)+":N"
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -25,3 +25,6 @@ class TestMarsRover:
     ])
     def test_command_forward_after_wrapping_around_return(self, mars_rover, command, expected):
         assert mars_rover.execute(command) == expected
+
+    def test_rotate_right_returns_0_0_E(self, mars_rover):
+        assert mars_rover.execute("R") == "0:0:E"
