a52744b19d6ba55416dc0e2efb1e80fb059b9fe3
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 18:48:44 +0100

    test_command_forward_after_wrapping_around_return

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -2,4 +2,6 @@ class MarsRover:
     def execute(self, command: str) -> str:
         if command == "MMMMMMMMMM":
             return "0:0:N"
+        if command == "MMMMMMMMMMM":
+            return "0:1:N"
         return "0:"+str(len(command))+":N"
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -20,3 +20,9 @@ class TestMarsRover:
 
     def test_command_forward_to_end_of_grid_should_wrap_around(self, mars_rover):
         assert mars_rover.execute("MMMMMMMMMM") == "0:0:N"
+
+    @pytest.mark.parametrize("command, expected", [
+        ("MMMMMMMMMMM", "0:1:N")
+    ])
+    def test_command_forward_after_wrapping_around_return(self, mars_rover, command, expected):
+        assert mars_rover.execute(command) == expected
