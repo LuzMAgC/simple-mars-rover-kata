185332a66715a18a2db3ba3bce95c6033dedaf79
luz_maria <luz.maria.agc@gmail.com>
2023-12-03 18:33:53 +0100

    refactor: parametrize test_command_forward_return

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -10,11 +10,10 @@ class TestMarsRover:
     def test_landing_position_returns_0_0_N(self, mars_rover):
         assert mars_rover.execute("") == "0:0:N"
 
-    def test_command_forward_returns_0_1_N(self, mars_rover):
-        assert mars_rover.execute("M") == "0:1:N"
-
-    def test_command_forward_twice_returns_0_2_N(self, mars_rover):
-        assert mars_rover.execute("MM") == "0:2:N"
-
-    def test_command_forward_three_times_returns_0_3_N(self, mars_rover):
-        assert mars_rover.execute("MMM") == "0:3:N"
+    @pytest.mark.parametrize("command, expected", [
+        ("M", "0:1:N"),
+        ("MM", "0:2:N"),
+        ("MMM", "0:3:N")
+    ])
+    def test_command_forward_return(self, mars_rover, command, expected):
+        assert mars_rover.execute(command) == expected
