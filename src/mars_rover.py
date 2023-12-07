dc98cf7e5a81395c79afa6b5d334e39fd182ec83
luz_maria <luz.maria.agc@gmail.com>
2023-12-07 14:55:55 +0100

    refactor: make internal properties private

--- src/mars_rover.py
+++ src/mars_rover.py
@@ -3,32 +3,32 @@ from src.grid import Grid
 
 class MarsRover:
     __grid: Grid
-    directions = ["N", "E", "S", "W"]
-    move_forward_command = "M"
-    turn_right_command = "R"
-    turn_left_command = "L"
+    __directions = ["N", "E", "S", "W"]
+    __move_forward_command = "M"
+    __turn_right_command = "R"
+    __turn_left_command = "L"
 
     def __init__(self, grid: Grid):
         self.__grid = grid
 
     def execute(self, command: str) -> str:
-        direction = self.directions[0]
+        direction = self.__directions[0]
         y_axis = 0
         x_axis = 0
 
         for instruction in command:
-            if instruction == self.turn_right_command:
+            if instruction == self.__turn_right_command:
                 direction = self.__turn_right(direction)
 
-            if instruction == self.turn_left_command:
+            if instruction == self.__turn_left_command:
                 direction = self.__turn_left(direction)
 
-            if instruction == self.move_forward_command:
-                if direction == self.directions[0]:
+            if instruction == self.__move_forward_command:
+                if direction == self.__directions[0]:
                     y_axis += 1
-                elif direction == self.directions[1]:
+                elif direction == self.__directions[1]:
                     x_axis += 1
-                elif direction == self.directions[2]:
+                elif direction == self.__directions[2]:
                     y_axis -= 1
                 else:
                     x_axis -= 1
@@ -36,7 +36,7 @@ class MarsRover:
         return str(x_axis % self.__grid.width()) + ":" + str(y_axis % self.__grid.height()) + ":" + direction
 
     def __turn_left(self, direction: str) -> str:
-        return self.directions[(self.directions.index(direction) - 1) % len(self.directions)]
+        return self.__directions[(self.__directions.index(direction) - 1) % len(self.__directions)]
 
     def __turn_right(self, direction: str) -> str:
-        return self.directions[(self.directions.index(direction) + 1) % len(self.directions)]
+        return self.__directions[(self.__directions.index(direction) + 1) % len(self.__directions)]
